90a80d4052b64d9cc0dd2bce7a2c73a8ebb066eb
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-05-07 18:19:12 -0500

    successfully tested route and add http file to the data base

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/actors/:id" => "actors#show"
+
+  get "movies" => "movies#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 Actor.create({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
 
+Movie.create({title: "The lord of the rings", year: 2001, plot: "A saga of a group of sometimes reluctant heros who set forth to save thier world from consummate evil. Its many worlds and creatures were drawn from Tolkien's extensive knowlegde of philology and folklore."})
