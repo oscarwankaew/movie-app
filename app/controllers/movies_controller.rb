02a21dddb7e542d99aade2cde6cf277e2e5b57ea
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-05-09 20:39:32 -0500

    adds patch update and delete destroy to movie model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,4 +18,19 @@ class MoviesController < ApplicationController
         movie.save
         render json: movie.as_json
     end
+
+    def update
+        movie = Movie.find_by(id: params["id"])
+        movie.title = params["title"] || movie.title
+        movie.year = params["year"] || movie.year
+        movie.plot = params["plot"] || movie.plot
+        movie.save
+        render json: movie.as_json
+    end
+
+    def destroy
+        movie = Movie.find_by(id: params["id"])
+        movie.destroy
+        render json: { message: "Selected movie has been deleted." }
+    end
 end
