3b175e24037349dc9570bffc1f36106df531a358
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-05-11 18:43:44 -0500

    adds director and english attributes to Movie model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_07_230210) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_11_233956) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_07_230210) do
     t.text "plot"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "director"
+    t.string "english"
   end
 
 end
