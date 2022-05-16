bc60899d1762592837c5fe84fea33a4b7813b6c3
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-05-16 10:47:16 -0500

    fixes minimum validation

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,8 +1,8 @@
 class Actor < ApplicationRecord
   validates :first_name, presence: true
-  validates :first_name, length: { minimun: 2 }
+  validates :first_name, length: { minimum: 2 }
   validates :last_name, presence: true
-  validates :last_name, length: { minimun: 2 }
+  validates :last_name, length: { minimum: 2 }
   validates :known_for, presence: true
-  validates :age, numericality: { minimun: 13 }
+  validates :age, numericality: { minimum: 13 }
 end
