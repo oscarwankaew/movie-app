a9dd3ecca883b316178ac2e8574bbd24067070c5
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-05-21 14:58:17 -0500

    Adds relations between movie and genre through movie_genres

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,6 @@ class Movie < ApplicationRecord
   validates :plot, length: { minimum: 50 }
   validates :director, presence: true
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
